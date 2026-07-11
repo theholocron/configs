5780111d649897782ea333a7295cca44ade42ea6
Newton <5769156+iamnewton@users.noreply.github.com>
2026-07-11 09:43:55 -0700

    fix(eslint-config): re-export all presets from root index.js
    index.js only exported base and typescript, but the README showed all 8
    presets importable from the root. Add the missing re-exports so consumers
    can use named imports from @theholocron/eslint-config directly without
    needing subpath imports for node, react, next, vitest, storybook, cypress.
    
    Update root eslint.config.js to use the consolidated root import.
    
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,2 +1,8 @@
 export { base } from "./configs/base.js";
 export { typescript } from "./configs/typescript.js";
+export { react } from "./configs/react.js";
+export { next } from "./configs/next.js";
+export { node } from "./configs/node.js";
+export { vitest } from "./configs/vitest.js";
+export { storybook } from "./configs/storybook.js";
+export { cypress } from "./configs/cypress.js";
