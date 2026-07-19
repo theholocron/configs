1bf8b717115f01119920a14f38146c5d43a79bc5
iamnewton <5769156+iamnewton@users.noreply.github.com>
2026-07-19 03:45:26 +0000

    chore: fix linting issues
    Signed-off-by: super-linter <super-linter@super-linter.dev>

--- packages/eslint-config/configs/storybook.ts
+++ packages/eslint-config/configs/storybook.ts
@@ -2,7 +2,9 @@ import storybook from "eslint-plugin-storybook";
 import type { Linter } from "eslint";
 
 export function storybookConfig(): Linter.FlatConfig[] {
-	return [...storybook.configs["flat/recommended"]] as unknown as Linter.FlatConfig[];
+	return [
+		...storybook.configs["flat/recommended"],
+	] as unknown as Linter.FlatConfig[];
 }
 
 export { storybookConfig as storybook };
