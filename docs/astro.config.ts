a290ae2f3775c295ca4773cea18a1717096d1321
Newton <5769156+iamnewton@users.noreply.github.com>
2026-08-01 17:19:26 -0700

    fix(docs): use workspace:* for astro-config and update to new API
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,4 +1,11 @@
+import starlight from "@astrojs/starlight";
+import { docsTheme } from "@theholocron/docs-theme";
 import { defineConfig } from "@theholocron/astro-config";
 import configsConfig from "@theholocron/configs-docs";
 
-export default defineConfig({ docs: configsConfig, importMetaUrl: import.meta.url });
+export default defineConfig({
+	docs: configsConfig,
+	importMetaUrl: import.meta.url,
+	starlight,
+	docsTheme,
+});
